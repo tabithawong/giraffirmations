507ea12a9801469d142e8937995dead0ded11912
Tabitha Wong <tabi159w@gmail.com>
2021-01-30 11:11:00 -0500

    quotes

--- browserAction/src/scripts/popup.js
+++ browserAction/src/scripts/popup.js
@@ -24,3 +24,12 @@ function openTab(evt, tabName) {
     evt.currentTarget.className += " active";
 }
 
+// array of quotes
+var quotes = ["I am grateful for...", "What would make today great?", "Daily affirmations: I am...", "3 Amazing things that happened today", "How could I have made today better?",
+"What am I best at?", "What do I love doing the most?", "What are you excited about?", "LWhat do you love most about yourself?", "Who do you admire most in the world and why?", "What are 5 bucket list items for this year?", "What is your favourite food?", "What is your favourite song?", "What about your life makes you most proud?", "What is a challenge that you overcame?", "What three things could you give up that would give you more time, energy, and peace?"];
+
+// random quote index
+const quotes_i = Math.floor(Math.random() * quotes.length);
+// set a quote
+document.getElementById("quote").innerHTML = quotes[quotes_i];
+
