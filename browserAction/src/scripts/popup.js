97a07e187c03206b61848bcfea8e072d1ced248b
Mabel Luo <mabel.luom@gmail.com>
2021-01-30 15:51:53 -0500

    Revert here!!

--- browserAction/src/scripts/popup.js
+++ browserAction/src/scripts/popup.js
@@ -48,7 +48,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
 // array of quotes
 var quotes = ["I am grateful for...", "What would make today great?", "Daily affirmations: I am...", "What are 3 amazing things that happened today?", "How could you have made today better?",
-"What am you best at?", "What do you love doing the most?", "What are you excited about?", "What do you love most about yourself?", "Who do you admire most in the world and why?", "What are 5 bucket list items for this year?", "What is your favourite food?", "What is your favourite song?", "What about your life makes you most proud?", "What is a challenge that you overcame?", "What three things could you give up that would give you more time, energy, and peace?"];
+"What am you best at?", "What do you love doing the most?", "What are you excited about?", "What do you love most about yourself?", "Who do you admire most in the world and why?", "What are 5 bucket list items for this year?", "What is your favourite food?", "What is your favourite song?", "What about your life makes you most proud?", "What is a challenge that you overcame?", "What three things could you give up that would give you more time, energy, and peace?", "Who are you grateful for?", "What activities and hobbies would you miss if you were unable to do them?", "What skills or abilities are you thankful to have?", "What about the city you live in are you grateful for?", "How are things in your life today different than a year ago? What positive changes are you thankful for?"];
 
 // random quote index
 const quotes_i = Math.floor(Math.random() * quotes.length);
