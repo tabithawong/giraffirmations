c1bfbcdaaeb59bd780ee0126f91f530fd5ee6729
Tabitha Wong <tabi159w@gmail.com>
2021-01-30 11:27:13 -0500

    restart

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-# gratatouille
+# Chrome Extension Starter Repo
+
+> A Repo to make learning Chrome Extensions simple.
+
+## Table of Contents
+
+- [Background](#background)
+- [Installation](#installation)
+- [Further Reading](#further-reading)
+
+## Background
+This repo accompanies the following tech talk at The HX Project Lounge conference:
+
+https://docs.google.com/presentation/d/1wonYMXPqXZE7_cfnnrf47jcxnA-Z0wg0XFq__b4nDPo/edit?usp=sharing
+
+It is designed only to encourage experiementation with Chrome Extension development and provides only the most basic examples. It contains:
+
+## Extension Type A - A Browser Action Extension
+
+This type of extension appears in the toolbar of the browser next to the URL bar. It can perform functionality which acts both in it's own process and UI or interacts with the browser page, using a content script.
+
+## Extension Type B - A Page Action Extension
+
+This type of extension functionas as above but only appears active on certain sites as defined by the background script. This allows you to target just the sites you want to execute the extension behaviour on.
+
+## Installation
+
+Clone this repo and then go to chrome://extensions/ and then Load Unpacked and navigate to the src/ folder of the extenxion and load it from source. The extension should appear in the browser.
+
+### Further Reading
+
+https://developer.chrome.com/extensions/manifest
+
+https://code.tutsplus.com/tutorials/developing-google-chrome-extensions--net-33076
+
+http://waitingphoenix.com/how-to-make-your-chrome-extension-access-webpage/
+
+https://www.youtube.com/watch?v=vnXb1S6l_jE
--- browserAction/src/scripts/popup.js
+++ browserAction/src/scripts/popup.js
@@ -1,35 +1,16 @@
+function isFrogHidden() {
+    const frog = document.getElementById('frog')
+    const style = window.getComputedStyle(frog)
+    frog.style.display = (style.display === 'none') ? 'block' : 'none'
+}
+
 function fireContentScript() {
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         chrome.tabs.sendMessage(tabs[0].id, { fireContentScript: true, type: 'fireContentScript' })
     })
 }
-function openTab(evt, tabName) {
-    // Declare all variables
-    var i, tabcontent, tablinks;
-  
-    // Get all elements with class="tabcontent" and hide them
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-    }
-  
-    // Get all elements with class="tablinks" and remove the class "active"
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-  
-    // Show the current tab, and add an "active" class to the button that opened the tab
-    document.getElementById(tabName).style.display = "block";
-    evt.currentTarget.className += " active";
-}
 
-// array of quotes
-var quotes = ["I am grateful for...", "What would make today great?", "Daily affirmations: I am...", "3 Amazing things that happened today", "How could I have made today better?",
-"What am I best at?", "What do I love doing the most?", "What are you excited about?", "LWhat do you love most about yourself?", "Who do you admire most in the world and why?", "What are 5 bucket list items for this year?", "What is your favourite food?", "What is your favourite song?", "What about your life makes you most proud?", "What is a challenge that you overcame?", "What three things could you give up that would give you more time, energy, and peace?"];
-
-// random quote index
-const quotes_i = Math.floor(Math.random() * quotes.length);
-// set a quote
-document.getElementById("quote").innerHTML = quotes[quotes_i];
-
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('clickMe').addEventListener("click", isFrogHidden)
+    document.getElementById('fireContentScript').addEventListener("click", fireContentScript)
+})
