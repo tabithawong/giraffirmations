2f285dd71a6a72d14f1e93e116e4d200c8ccf0e7
Mabel Luo <mabel.luom@gmail.com>
2021-01-30 15:37:33 -0500

    Add prompt to saveEntry

--- browserAction/src/scripts/popup.js
+++ browserAction/src/scripts/popup.js
@@ -10,9 +10,10 @@ function saveEntry() {
     if (document.getElementById("saveEntry").innerHTML != "Saved") {
         document.getElementById("saveEntry").innerHTML = "Saved";
         var entry = document.getElementById("entry").value;
+        var prompt = document.getElementById("quote").value;
         var date = new Date();
         var currentdate = (date.getMonth() + 1) + "/" + date.getDate() + "/" + date.getUTCFullYear();
-        dayEntries.push(currentdate + " " + entry);
+        dayEntries.push(currentdate + " " + prompt + " " + entry);
         console.log(dayEntries);
         document.getElementById("entry").value = "";
     }
